e49978f1414c73541981709aad3c1d3508daba84
vamsikrishna-1508 <76095598+vamsikrishna-1508@users.noreply.github.com>
2021-02-27 16:24:10 +0530

    Initial commit

---
+++ src/Main.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import { Card } from "react-bootstrap";
+import { data } from "./utils";
+
+const initialColors = Object.keys(data);
+
+const Main = () => {
+  const [colors, setColors] = useState(initialColors);
+
+  const handleChange = (e) => {
+    const value = e?.target?.value;
+    const filteredColors = initialColors.filter(
+      (color) => color.toLowerCase().indexOf(value.toLowerCase()) > -1
+    );
+    setColors(filteredColors);
+  };
+
+  return (
+    <div>
+      <input className="form-control m-2 w-50 d-flex" onChange={handleChange} />
+      <div className="row">
+        {colors.map((color, idx) => (
+          <ColorCard key={idx} color={color} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const ColorCard = ({ color }) => {
+  const style = {
+    width: "12rem",
+    display: "flex",
+    margin: "22px",
+    backgroundColor: data[color],
+    color: color === "black" ? "#fff" : "black",
+    height: "20vh",
+    border: "1px solid #000",
+    cursor: "pointer"
+  };
+
+  const handleClick = () => {
+    document.body.style.backgroundColor = color;
+  };
+
+  return (
+    <>
+      <Card style={style} onClick={handleClick}>
+        <Card.Body>{color}</Card.Body>
+      </Card>
+    </>
+  );
+};
+
+export default Main;
